f510e45e97f7f5fb2d3394baac80aea0df001668
Alexandre Gomez Martins <121064210+alexandregomezm@users.noreply.github.com>
2024-10-26 14:51:40 -0300

    twoSum neetcode way

--- array&hashing/two-sum.py
+++ array&hashing/two-sum.py
@@ -3,17 +3,15 @@ import random
 class Solution:
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    ans = [i, j]
-                    return ans
-                else:
-                    print("Não atingiu o target.")
+        values = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in values:
+                return [values[diff], i]
+            values[n] = i
 
-# nums = [random.randint(1,10) for _ in range (5)]  #that array wil have 5 numbers, all sorted from 1 to 10.
 nums = [3,4,5,6]
 target = 7
 sol = Solution()
-twoSum = sol.twoSum(nums, target)
-print(twoSum)
+ans = sol.twoSum(nums, target)
+print(ans)
