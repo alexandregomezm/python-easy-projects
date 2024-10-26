7c5cd7c6978acc53c66f04ae976671009bf298f8
Alexandre Gomez Martins <121064210+alexandregomezm@users.noreply.github.com>
2024-10-26 14:59:31 -0300

    alt2

--- array&hashing/two-sum.py
+++ array&hashing/two-sum.py
@@ -7,9 +7,9 @@ class Solution:
         for i, n in enumerate(nums):
             diff = target - n
             if diff in values:
-                return [values[diff], i]
+                return [values[diff], i]  # return the index of the diff value, and the index that gets that especifficaly diff value
             values[n] = i # values = {3:0, 4:1, 5:2, 6:3}   {n : i}
-            
+
 nums = [3,4,5,6]
 target = 7
 sol = Solution()
